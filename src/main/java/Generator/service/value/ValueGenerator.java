13800c236a78a8007854b445c6712e43efc6cbad
PiotrKulesza <piotrkulesza16@onet.pl>
2020-08-20 16:53:04 +0200

    small changes 3

--- src/main/java/Generator/service/value/ValueGenerator.java
+++ src/main/java/Generator/service/value/ValueGenerator.java
@@ -9,7 +9,7 @@ public class ValueGenerator {
 
 
     public static double generateValue() throws IOException{
-    	Process p = Runtime.getRuntime().exec("ps -A -o pcpu | tail -n+2 | paste -sd+ | bc");                                                                                                                                                     
+    	Process p = Runtime.getRuntime().exec("sudo ps -A -o pcpu | tail -n+2 | paste -sd+ | bc");                                                                                                                                                     
     	BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
     	
     	
